2ea74cb4516adf9a3979da14b4f361746f084c40
João Henrique <jhenrique.caval@hotmail.com>
2026-02-01 23:19:08 -0300

    fix: mudando migration para postgresql

--- src/main/resources/db/migration/V1__create_users_and_cards_tables.sql
+++ src/main/resources/db/migration/V1__create_users_and_cards_tables.sql
@@ -1,27 +1,17 @@
-
 CREATE TABLE users (
-                       id BIGINT BIGSERIAL  PRIMARY KEY,
-
+                       id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                        nome VARCHAR(100) NOT NULL,
-
                        email VARCHAR(150) NOT NULL UNIQUE,
-
                        senha VARCHAR(255) NOT NULL,
-
                        role VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE cards (
-                       id BIGINT BIGSERIAL  PRIMARY KEY,
-
+                       id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                        numero_cartao BIGINT NOT NULL,
-
                        nome VARCHAR(100) NOT NULL,
-
                        status BOOLEAN NOT NULL,
-
                        tipo_cartao VARCHAR(50) NOT NULL,
-
                        user_id BIGINT NOT NULL,
 
                        CONSTRAINT fk_cards_user
